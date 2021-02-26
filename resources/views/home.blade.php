52f9f26ffc29bb7671573dc2d2b528f69883a810
Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2021-02-26 17:59:47 +0100

    add dump db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Welcome') }}</div>
 
                 <div class="card-body">
                     <div class="jumbotron">
